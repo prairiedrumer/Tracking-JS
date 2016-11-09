5d6c9746f718cf18962d2a3efd355fe7dc7c472b
Sam <prairiedrumer@gmail.com>
2016-11-09 10:07:43 -0600

    Add files via upload
    Add Tracking JS

---
+++ Tracking/analytics-tracking-test.js
@@ -0,0 +1,185 @@
+﻿
+describe("When tracking,", function () {
+
+    var sut;
+    var id = "612007";
+    var productIds = [612007, 562717];
+    var name = "Badger Gun";
+    var listName = "Product | Recommendations";
+    var productQuantity = "1";
+    var position = "1";
+    var productPrice = "200";
+    var category = "657";
+    var brand = "Badger Ordnance";
+    var action = "detail";
+    var typeEvent = "UX";
+    var eventAction = "click";
+    var stringAction = {'step': window.googleCheckoutStep};
+    var actionName = "add to cart";
+
+    beforeEach(function () {
+        var analyticsTrack = new analyticsTracking();
+        sut = analyticsTrack;
+        //spyOn(console, 'log').and.callThrough();
+        spyOn(console, 'log');
+    });
+
+    describe("and GA and FBQ are not define", function() {
+        describe("with Goole Analytics", function () {
+
+            it("should call console.log with GA Not defined.", function () {
+                sut.googleAnalyticsAddProduct(id, name, null, category, brand);
+                expect(console.log).toHaveBeenCalledWith("GA Not defined");
+            });
+           
+            it("should call Ga Set Action", function () {
+                sut.googleAnalyticsSetAction(action, null);
+                expect(console.log).toHaveBeenCalledWith("GA Not defined");
+            });
+          
+            it("should call Ga Event", function () {
+                sut.googleAnalyticsEvent(typeEvent, eventAction, actionName);
+                expect(console.log).toHaveBeenCalledWith("GA Not defined");
+            });
+
+            it("should call Ga Pageview", function () {
+                sut.googleAnalyticsPageview();
+                expect(console.log).toHaveBeenCalledWith("GA Not defined");
+            });
+
+            it("should call GA Add Impression", function () {
+                sut.googleAnalyticsAddImpression(id, name, listName, position);
+                expect(console.log).toHaveBeenCalledWith("GA Not defined");
+            });
+        });
+        describe("with Facebook ", function () {
+            it("should call fbq View Content", function () {
+                sut.facebookViewContent(id);
+                expect(console.log).toHaveBeenCalledWith("FBQ Not defined");
+            });
+            it("should call fbq Add To Cart", function () {
+                sut.facebookAddProduct(id);
+                expect(console.log).toHaveBeenCalledWith("FBQ Not defined");
+            });
+        });
+    });
+    describe("When GA and FBQ are define", function () {
+
+        beforeEach(function() {
+            ga = jasmine.createSpy();
+            fbq = jasmine.createSpy();
+        });
+
+        describe("a product list", function () {
+            var localDocument;
+            var localWindow;
+            beforeEach(function () {
+                localDocument = { URL: "gfsegesg" };
+            });
+
+            describe("and its on a product page", function () {
+                beforeEach(function () {
+                    localDocument = { URL: "gfsegesg/product/" };
+                });
+                it("should return Product | Recommendations", function () {
+                    expect(sut.getFullListNamePrivate('Recommendations', localDocument, localWindow)).toBe('Product | Recommendations');
+                });
+            });
+
+            describe("and its on a Home Page", function () {
+                beforeEach(function () {
+                    localWindow = { location: { pathname: "/" } };
+                });
+                it("should return Home | Recommendations", function () {
+                    expect(sut.getFullListNamePrivate('Recommendations', localDocument, localWindow)).toBe('Homepage | Recommendations');
+                });
+            });
+            describe("and its on a Sale Page", function () {
+                beforeEach(function () {
+                    localWindow = { location: { pathname: "/sale" } };
+                });
+                it("should return Sale | Recommendations", function () {
+                    expect(sut.getFullListNamePrivate('Recommendations', localDocument, localWindow)).toBeDefined();
+                });
+            });
+
+        });
+        describe("with Goole Analytics", function () {
+
+            it("should call Ga Add Product with Category and Brand", function () {
+                sut.googleAnalyticsAddProduct(id, name, null, category, brand);
+                expect(ga).toHaveBeenCalledWith("ec:addProduct", { "id": id, "name": name, "category": category, "brand": brand });
+                expect(ga.calls.count()).toBe(1);
+            });
+
+            it("should call Ga Add Product with Position", function () {
+                sut.googleAnalyticsAddProduct(id, name, null, null, null, position);
+                expect(ga).toHaveBeenCalledWith("ec:addProduct", { "id": id, "name": name, "position": position });
+                expect(ga.calls.count()).toBe(1); 
+            });
+
+            it("should call Ga Add Product with Quantity", function () {
+                sut.googleAnalyticsAddProduct(id, name, productQuantity);
+                expect(ga).toHaveBeenCalledWith("ec:addProduct", { "id": id, "name": name, "quantity": productQuantity });
+                expect(ga.calls.count()).toBe(1);
+            });
+
+            it("should call Ga Add Product with Price and Quantity", function () {
+                sut.googleAnalyticsAddProduct(id, name, productQuantity, null, null, null, productPrice);
+                expect(ga).toHaveBeenCalledWith("ec:addProduct", { "id": id, "name": name, "price": productPrice, "quantity": productQuantity });
+                expect(ga.calls.count()).toBe(1);
+            });
+
+            it("should call Ga Set Action", function () {
+                sut.googleAnalyticsSetAction(action, null);
+                expect(ga).toHaveBeenCalledWith("ec:setAction", action);
+                expect(ga.calls.count()).toBe(1);
+            });
+
+            it("should call Ga Set Action with a actionString", function () {
+                sut.googleAnalyticsSetAction(action, stringAction);
+                expect(ga).toHaveBeenCalledWith("ec:setAction", action, stringAction);
+                expect(ga.calls.count()).toBe(1);
+            });
+
+            it("should call Ga Event", function () {
+                sut.googleAnalyticsEvent(typeEvent, eventAction, actionName);
+                expect(ga).toHaveBeenCalledWith("send", "event", typeEvent, eventAction, actionName);
+                expect(ga.calls.count()).toBe(1);
+            });
+
+            it("should call Ga Pageview", function () {
+                sut.googleAnalyticsPageview();
+                expect(ga).toHaveBeenCalledWith("send", "pageview");
+                expect(ga.calls.count()).toBe(1);
+            });
+
+            it("should call GA Add Impression", function () {
+                sut.googleAnalyticsAddImpression(id, name, listName, position);
+                expect(ga).toHaveBeenCalledWith("ec:addImpression", { "id": id, "name": name, "list": listName, "position": position });
+                expect(ga.calls.count()).toBe(1);
+            });
+        });
+        describe("with Facebook ", function () {
+            it("should call fbq View Content for single product", function () {
+                sut.facebookViewContent(id);
+                expect(fbq).toHaveBeenCalledWith("track", "ViewContent", { 'content_ids': id, 'content_type': "product" });
+                expect(fbq.calls.count()).toBe(1);
+            });
+
+            it("should call fbq View Content with Multi products", function () {
+                sut.facebookViewContent(productIds);
+                expect(fbq).toHaveBeenCalledWith("track", "ViewContent", { 'content_ids': productIds, 'content_type': "product" });
+                expect(fbq.calls.count()).toBe(1);
+            });
+
+            it("should call fbq Add To Cart", function () {
+                sut.facebookAddProduct(id);
+                expect(fbq).toHaveBeenCalledWith("track", "AddToCart", { 'content_ids': id, 'content_type': "product" });
+                expect(fbq.calls.count()).toBe(1);
+            });
+        });
+    });
+
+});
+
---
+++ Tracking/analytics-tracking.js
@@ -0,0 +1,121 @@
+﻿var analyticsTracking = (function () {
+    "use strict";
+    function analyticsTracking() {
+
+    }
+
+    analyticsTracking.prototype.getFullListName = function (listing) {
+        var localDocument = document;
+        var localWindow = window;
+        return this.getFullListNamePrivate(listing, localDocument, localWindow);
+    }
+
+    analyticsTracking.prototype.getFullListNamePrivate = function (listing, localDocument, localWindow) {
+        var isOnProductPage = localDocument.URL.indexOf("/product/") !== -1;
+        if (isOnProductPage) {
+            var productPageListing = "Product | " + listing;
+            return productPageListing;
+        }
+
+        var listName = "";
+        var path = localWindow.location.pathname.split("/");
+        for (var x = 1; x < path.length; x++) {
+
+            var pathSection = path[x];
+            var isOnHomepage = pathSection.length === 0;
+            if (isOnHomepage) {
+                pathSection = "Homepage";
+            } else {
+                pathSection = pathSection.charAt(0).toUpperCase() + pathSection.slice(1);
+            }
+
+            listName += pathSection + " | ";
+        }
+
+        listName += listing;
+
+        return listName;
+    }
+
+    analyticsTracking.prototype.googleAnalyticsEvent = function (typeOfAction, action, eventName, actionString) {
+        if (actionString === null) {
+            safeGa("send", "event", typeOfAction, action, eventName);
+        } else {
+            safeGa("send", "event", typeOfAction, action, eventName, actionString);
+        }
+    }
+
+    analyticsTracking.prototype.googleAnalyticsPromotion = function (promotionId, promotionName, position) {
+        safeGa("ec:addPromo", { 'id': promotionId, 'name': promotionName, 'position': position });
+    }
+
+    analyticsTracking.prototype.googleAnalyticsSetAction = function (action, actionString) {
+        if (actionString === null) {
+            safeGa("ec:setAction", action);
+        } else {
+            safeGa("ec:setAction", action, actionString);
+        }
+    }
+
+    analyticsTracking.prototype.googleAnalyticsAddProduct = function (productId, productName, productQty, productCategory, productbrand, productPosition, productPrice) {
+        var obj = {
+            'id': productId,
+            'name': productName
+        }
+
+        if (productCategory) obj.category = productCategory;
+        if (productQty) obj.quantity = productQty;
+        if (productbrand) obj.brand = productbrand;
+        if (productPosition) obj.position = productPosition;
+        if (productPrice) obj.price = productPrice;
+
+        safeGa("ec:addProduct", obj);
+    }
+
+    analyticsTracking.prototype.googleAnalyticsAddImpression = function (productId, productName, listName, productPosition) {
+        safeGa("ec:addImpression", {
+            "id": productId,
+            "name": productName,
+            "list": listName,
+            "position": productPosition
+        });
+    }
+
+    analyticsTracking.prototype.googleAnalyticsPageview = function () {
+        safeGa("send", "pageview");
+    }
+
+    analyticsTracking.prototype.facebookViewContent = function (products) {
+        safeFbq("track", "ViewContent", {
+            'content_ids': products,
+            'content_type': "product"
+        });
+    }
+
+    analyticsTracking.prototype.facebookAddProduct = function (product) {
+        safeFbq("track", "AddToCart", {
+            'content_ids': product,
+            'content_type': "product"
+        });
+    }
+
+    function safeFbq(fbqParamOne, fbqParamTwo, fbqContentObject) {
+        if (window.fbq === undefined) {
+            console.log("FBQ Not defined");
+        } else {
+            fbq(fbqParamOne, fbqParamTwo, fbqContentObject);
+        }
+    };
+
+    function safeGa(gaParamOne, gaParamTwo, typeOfAction, action, eventName) {
+        if (window.ga !== undefined) {
+            if (eventName !== undefined) { ga(gaParamOne, gaParamTwo, typeOfAction, action, eventName); }
+            else if (typeOfAction !== undefined) { ga(gaParamOne, gaParamTwo, typeOfAction); }
+            else if (gaParamTwo !== undefined) { ga(gaParamOne, gaParamTwo); }
+        } else {
+            console.log("GA Not defined");
+        }
+    };
+
+    return analyticsTracking;
+})();
